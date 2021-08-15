aaf54cec8180241a987e9483f60acd256f1a2d38
Evgeny <vasilkalin@gmail.com>
2021-08-15 23:09:52 +0200

    Work not all

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -5,7 +5,6 @@ import lombok.Setter;
 import lombok.ToString;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
